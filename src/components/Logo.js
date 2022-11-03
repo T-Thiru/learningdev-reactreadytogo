7b148ad11dca44e6df01c47f4f6cee1d7f013353
Thiruvaran Thayalan <110049957+T-Thiru@users.noreply.github.com>
2022-11-03 10:55:38 +0100

    animation scale

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,12 +3,14 @@ import logo from "../img/rocket.svg";
 
 const Logo = (props) => {
   return (
-    <div
-      className={`container-logo ${
-        props.switch1 & props.switch2 & props.switch3 && "logoAnimation"
-      }`}
-    >
-      <img className="logo" src={logo} alt="" />
+    <div className="container-logo">
+      <img
+        className={`logo ${
+          props.switch1 & props.switch2 & props.switch3 && "logoAnimation"
+        }`}
+        src={logo}
+        alt=""
+      />
       <p>Ready to Go</p>
     </div>
   );
